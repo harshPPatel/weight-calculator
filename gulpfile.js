66e7e7196937919f354750ec206911d1528db44a
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2019-05-22 15:04:16 -0500

    Sass Production and Development Tasks created

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,22 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+sass.compiler = require('node-sass');
 
 // Source Paths
 const javaScriptSource = './source/js/*.js';
+const sassSource = './source/sass/**/*.scss';
 
 // Dewstination Paths
 const javaScriptDestination = './public/js/';
+const sassDestination = './public/css/';
 
 // Deveopment Process - JavaScript Task
-function appJS(cb) {
+function javaScriptDevlopmentBuild(cb) {
   return src(javaScriptSource)
     .pipe(concat('app.js'))
     .pipe(rename({
@@ -23,7 +30,7 @@ function appJS(cb) {
 }
 
 // Production Process - JavaScript Task
-function compressJavaScript(cb) {
+function javaScriptProductionBuild(cb) {
   return src(javaScriptSource)
     .pipe(concat('app.js'))
     .pipe(babel({
@@ -37,6 +44,32 @@ function compressJavaScript(cb) {
   cb();
 }
 
+function sassDevelopmentBuild(cb) {
+  return src(sassSource)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(dest(sassDestination));
+  cb();
+}
+
+function sassProductionBuild(cb) {
+  return src(sassSource)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ["cover 99.5%"],
+    }))
+    .pipe(cleanCSS({
+      compatibility: 'ie8',
+    }))
+    .pipe(rename({
+      extname: '.min.css',
+    }))
+    .pipe(dest(sassDestination));
+  cb();
+}
+
 // Exporting Tasks
-exports.build = series(compressJavaScript);
-exports.default = series(appJS);
+exports.build = series(javaScriptProductionBuild, sassProductionBuild);
+exports.default = series(javaScriptDevlopmentBuild, sassDevelopmentBuild);
