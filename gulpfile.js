48f01a13504a274f43deb5edc3a5b3d271970e82
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2019-05-22 18:21:25 -0500

    Watch Function Created

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Importing Modules
-const { series, src, dest } = require('gulp');
+const { series, src, dest, watch } = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
@@ -7,19 +7,22 @@ const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync');
 sass.compiler = require('node-sass');
 
 // Source Paths
 const javaScriptSource = './source/js/*.js';
 const vendorJavaScriptSource = './source/js/vendors/*.js';
 const sassSource = './source/sass/**/*.scss';
-const vendorCssSource = './source/css';
+const vendorCssSource = './source/css/*.css';
+const faviconSource = './source/favicon/*';
 
 // Dewstination Paths
 const javaScriptDestination = './public/js/';
 const vendorJavaScriptDestination = './public/js/vendors/';
 const sassDestination = './public/css/';
 const vendorCssDestination = './public/css/vendors/';
+const faviconDestination = './public/';
 
 // Deveopment Process - JavaScript Task
 function javaScriptDevlopmentBuild(cb) {
@@ -89,6 +92,42 @@ function vendorCss(cb) {
   cb();
 }
 
+// Favicon Tasks
+function favicon(cb) {
+  return src(faviconSource)
+    .pipe(dest(faviconDestination));
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  browserSync.init({
+    server: {
+      baseDir: './public'
+    },
+    notify: false,
+  });
+  watch(sassSource, sassDevelopmentBuild);
+  watch(javaScriptSource, javaScriptDevlopmentBuild);
+  watch(vendorJavaScriptSource, vendorJavaScript);
+  watch(vendorCssSource, vendorCss);
+  watch(faviconSource, favicon);
+  watch([
+    './public/*',
+    './public/js/*.js',
+    './public/js/vendors/*.js',
+    './public/css/*.css',
+    './public/css/vendors/*.css',
+  ]).on('change', browserSync.reload);;
+}
+
 // Exporting Tasks
-exports.build = series(javaScriptProductionBuild, sassProductionBuild, vendorCss, vendorJavaScript);
-exports.default = series(javaScriptDevlopmentBuild, sassDevelopmentBuild, vendorCss, vendorJavaScript);
+
+// Default Task
+exports.default = series(javaScriptDevlopmentBuild, sassDevelopmentBuild, vendorCss, vendorJavaScript, favicon);
+
+// Production Build Task
+exports.build = series(javaScriptProductionBuild, sassProductionBuild, vendorCss, vendorJavaScript, favicon);
+
+// Watch Task
+exports.watch = watchTask;
